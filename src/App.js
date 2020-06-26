3356b83383434d5383f70b520091e4cd2ecc6cf0
Roshan <roshan@hellofyllo.com>
2020-06-26 15:42:06 +0530

    updated with hardcoded host

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [isChatStarted, setIsChatStarted] = useState(false);
+  const [conversations, setConversations] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (isChatStarted) {
+      const source = new EventSource(`http://172.30.197.4:3500/chat/${userName}`);
+
+      source.addEventListener("message", (e) => {
+        setConversations((conversations) => conversations.concat([JSON.parse(e.data)]));
+      });
+    }
+  }, [isChatStarted]);
+
+  const onUserNameChange = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const onChatStart = () => {
+    setIsChatStarted(true);
+  };
+
+  const onMessageChange = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const onMessageSend = () => {
+    axios({
+      method: 'post',
+      url: 'http://172.30.197.4:3500/write',
+      headers: {
+        'content-type': 'application/json',
+      },
+      data: {
+        user: userName,
+        message
+      }
+    }).then(res => {
+      setMessage("");
+    });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={onUserNameChange} value={userName} />
+      <button disabled={!userName} type="button" onClick={onChatStart}>Start Chat</button>
+      <div>
+        <ul>
+          {conversations.map((item) => (
+            <li><b>{item.user}</b>{" "}<i>{item.message}</i> | {item.timeStamp}</li>
+          ))}
+        </ul>
+        <input disabled={!isChatStarted} type="text" value={message} onChange={onMessageChange} />
+        <button disabled={!message || !isChatStarted} type="button" onClick={onMessageSend}>Send</button>
+      </div>
     </div>
   );
 }
